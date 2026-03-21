534b621825cf5ed49bcecc0c7801d9c92ae02516
Arturo <arturoreyesgerman@gmail.com>
2026-03-20 18:08:41 -0600

    cambios

--- back/routes/web.php
+++ back/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/deploy-migrate', function () {
+    Artisan::call('migrate --force');
+    return "Tablas creadas con éxito: " . Artisan::output();
 });
