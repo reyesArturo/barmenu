f7a8b37706d94d58e34ab515936cfa7bfc0a1127
Arturo <arturoreyesgerman@gmail.com>
2026-03-23 13:28:27 -0600

    cambio de imagenes

--- back/app/Models/Product.php
+++ back/app/Models/Product.php
@@ -8,6 +8,21 @@ class Product extends Model
 {
     protected $fillable = ['category_id', 'name', 'description', 'price', 'image_url', 'is_available'];
 
+    public function getImageUrlAttribute(?string $value): ?string
+    {
+        if (!$value) {
+            return null;
+        }
+
+        if (str_starts_with($value, 'http://') || str_starts_with($value, 'https://')) {
+            return $value;
+        }
+
+        $normalizedPath = str_starts_with($value, '/') ? $value : '/' . $value;
+
+        return url($normalizedPath);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
